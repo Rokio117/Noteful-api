aebc9ed598e80620469fb46178eb1ebfe9ac1d24
Nick Hillary <hillan02@gmail.com>
2019-10-23 10:44:34 -0500

    CHANGED A FEW THINGS

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 
 const knex = require('knex');
-const { DB_URL } = require('../config');
+const { DATABASE_URL } = require('../config');
 const { PORT } = require('../config');
 
 const app = require('./app');
 
 const db = knex({
   client: 'pg',
-  connection: DB_URL
+  connection: DATABASE_URL
 });
 
 app.set('db', db);
